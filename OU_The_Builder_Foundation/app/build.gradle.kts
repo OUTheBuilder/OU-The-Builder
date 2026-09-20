bca52c975d260d315436a50489c863c008cd42a1
OUTheBuilder <01me01only01@gmail.com>
2026-09-21 01:32:28 +0800

    Update app Gradle dependencies formatting

--- OU_The_Builder_Foundation/app/build.gradle.kts
+++ OU_The_Builder_Foundation/app/build.gradle.kts
@@ -26,6 +26,7 @@ android {
 
 dependencies {
     val composeBom = platform("androidx.compose:compose-bom:2026.09.00")
+
     implementation(composeBom)
     androidTestImplementation(composeBom)
 
@@ -34,5 +35,6 @@ dependencies {
     implementation("androidx.compose.foundation:foundation")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
+
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
