04d7027f315276269bc8ed14a09a0b7f1e96d9e6
OUTheBuilder <01me01only01@gmail.com>
2026-09-21 01:22:35 +0800

    Set Android compile and target SDK to 35

--- OU_The_Builder_Foundation/app/build.gradle.kts
+++ OU_The_Builder_Foundation/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.oubuilder"
-    compileSdk = 37
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.oubuilder"
         minSdk = 26
-        targetSdk = 37
+        targetSdk = 35
         versionCode = 1
         versionName = "0.1.0"
     }
